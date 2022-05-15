ff54488c6c95be9869b26ac1f0567fcb36a7f579
cann484 <reilly.cannon@pnnl.gov>
2022-05-15 05:33:04 -0700

    benchmark seq block mvp

--- sequential_block_mvp/src/main.c
+++ sequential_block_mvp/src/main.c
@@ -17,18 +17,23 @@ main(int argc, char** argv)
 
     int num_blocks_in_matrix_row_col;
     int num_elements_in_block_row_col;
+    int num_iterations;
+    int iter;
     int result_size;
     int debug;
 
-    if (argc != 4){
-        print_string(stderr, "need three arguments: num_blocks_in_matrix_row_col, num_elements_in_block_row_col, and debug(0/1)");
+    if (argc != 5){
+        print_string( stderr
+                    , "need four arguments: num_blocks_in_matrix_row_col, num_elements_in_block_row_col, num_iterations, and debug(0/1)"
+                    );
     }
 
     else {
 
         num_blocks_in_matrix_row_col = atoi(argv[1]);
         num_elements_in_block_row_col = atoi(argv[2]);
-        debug = atoi(argv[3]);
+        num_iterations = atoi(argv[3]);
+        debug = atoi(argv[4]);
 
         build_block_matrix  ( &matrix
                             , num_blocks_in_matrix_row_col
@@ -47,27 +52,36 @@ main(int argc, char** argv)
                         , num_elements_in_block_row_col
                         );
 
-        run_block_mvp   ( &result
-                        , &matrix
-                        , &vector
-                        , num_blocks_in_matrix_row_col
-                        , num_elements_in_block_row_col
-                        );
-        
-        result_size = num_blocks_in_matrix_row_col * num_elements_in_block_row_col;
-
-        if (debug) {
-            debug = verify_result(&result, result_size);
-            if (!debug) {
-                print_string(stderr, "\nincorrect\n");
-            }
-            else {
-                print_string(stderr, "\ncorrect\n");
-            }
+        if ((!matrix) || (!vector) || (!result) ){
+            print_string(stderr, "matrix, vector, or result not allocated");
         }
-        //print_result(&result, result_size);
+        else {
 
-        print_string(stderr, "\ndone\n");
+            for (iter = 0; iter < num_iterations; iter++){
+
+                run_block_mvp   ( &result
+                                , &matrix
+                                , &vector
+                                , num_blocks_in_matrix_row_col
+                                , num_elements_in_block_row_col
+                                );
+            }
+        
+            result_size = num_blocks_in_matrix_row_col * num_elements_in_block_row_col;
+
+            if (debug) {
+                debug = verify_result(&result, result_size);
+                if (!debug) {
+                    print_string(stderr, "\nincorrect\n");
+                }
+                else {
+                    print_string(stderr, "\ncorrect\n");
+                }
+            }
+            //print_result(&result, result_size);
+
+            print_string(stderr, "\ndone\n");
+        }
     }
     return 0;
 }
